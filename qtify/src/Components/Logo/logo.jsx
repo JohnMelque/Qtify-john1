f917cb7d9a61db9aeb7bdd3381aca63a83f3b26a
John Melque <102472899+JohnMelque@users.noreply.github.com>
2023-10-17 09:55:25 +0530

    Clone of Spotify

---
+++ qtify/src/Components/Logo/logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logoimage from "../../assest/Logo.png";
+
+const logo = () => {
+  return (
+    <img src={Logoimage} alt="Qtify Logo"  width={67}/>
+  )
+}
+
+export default logo
