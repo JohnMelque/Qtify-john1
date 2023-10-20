dd3b09156b5d6845957b0895e134b81d685e9540
John Melque <102472899+JohnMelque@users.noreply.github.com>
2023-10-20 18:04:02 +0530

    qtify-top-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,37 +2,38 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { Hero } from './Components/Hero/Hero';
 import { Navbar } from './Components/NavBar/Navbar';
-import { featchTopAlbum,featchNewAlbum } from './api/api';
+import { featchTopAlbum } from './api/api';
 import Section from './Components/Section/Section';
 
 
 function App() {
   const [topalbum, setTobalbum] = useState([]);
-  const [newalbum, setNewalbum] = useState([]);
+  // const [newalbum, setNewalbum] = useState([]);
   // const [songs, setSongs] = useState([]);
 
   const generateTobalbum = async () => {
     const data = await featchTopAlbum();
     setTobalbum(data);
   }
-  const generateNewalbum = async () => {
-    const data1 = await featchNewAlbum();
-    setNewalbum(data1);
-  }
+  // const generateNewalbum = async () => {
+  //   const data1 = await featchNewAlbum();
+  //   setNewalbum(data1);
+  // }
   // const generateSongs = async () => {
   //   const data2 = await featchSongs();
   //   setSongs(data2);
   // }
   useEffect(()=>{
     generateTobalbum();
-    generateNewalbum();
-  },[])
+    // generateNewalbum();
+  }, [])
+  
   return (
     <div>
       <Navbar /> 
       <Hero /> 
       <Section data={topalbum} title="Top Albums" type="album" />
-      <Section data={newalbum} title="New Albums" type="album" />
+      {/* <Section data={newalbum} title="New Albums" type="album" /> */}
       {/* <Section data={songs} title="Songs" type="album" />     */}
     </div>
   );
